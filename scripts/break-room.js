574fba8aa19f3ee4f2e3a8571fcb477c981f7384
Yen <yenvu.2305@gmail.com>
2023-05-26 10:16:01 -0700

    Add Navigation/ Delete ":root"

---
+++ scripts/break-room.js
@@ -0,0 +1,19 @@
+// ------- HAMBURGER MENU TOGGLE -------
+const menuBtn = document.querySelector(".navbar-toggle");
+const closeBtn = document.querySelector(".navbar-close");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+        navigation.classList.remove("active");
+    });
+});
