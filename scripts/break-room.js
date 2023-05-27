aef2ec1dfd4ff5a8f5977244332660597d177125
Yen <yenvu.2305@gmail.com>
2023-05-27 13:55:02 -0700

    Fixed Navigation in CSS and JS

--- scripts/break-room.js
+++ scripts/break-room.js
@@ -1,19 +1,25 @@
 // ------- HAMBURGER MENU TOGGLE -------
-const menuBtn = document.querySelector(".nav-toggle");
-const closeBtn = document.querySelector(".nav-close");
-const navigation = document.querySelector(".navigation");
-const navItems = document.querySelectorAll(".nav-items a");
+const menuBtn = document.querySelector(".navbar-toggle");
+const closeBtn = document.querySelector(".navbar-close");
+const navMenuCtn = document.querySelector(".navbar-menu-container");
+const navItems = document.querySelectorAll(".navbar-menu .navbar-item");
 
 menuBtn.addEventListener("click", () => {
-    navigation.classList.add("active");
+    navMenuCtn.classList.add("active");
 });
 
 closeBtn.addEventListener("click", () => {
-    navigation.classList.remove("active");
+    navMenuCtn.classList.remove("active");
 });
 
 navItems.forEach((navItem) => {
     navItem.addEventListener("click", () => {
-        navigation.classList.remove("active");
+        navMenuCtn.classList.remove("active");
     });
+});
+
+// ------- STICKY NAVIGATION -------
+window.addEventListener("scroll", function() {
+    const navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("sticky", window.scrollY > 0);
 });
